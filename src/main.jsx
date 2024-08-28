079d40cd852c8c4768229114076654d770ca263a
Joseph Nma <josgolf@yahoo.ca>
2024-08-28 14:34:10 +0100

    netfy

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from "./Router.jsx";
 import "./index.scss";
-// import "./index.css";
 import { Provider } from "react-redux";
 import store from "./Redux/Store.jsx";
 import { ToastContainer } from "react-toastify";
